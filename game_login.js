c4ad040cf807574fa071aa40e941b21e56985898
nishthaaror <112774695+nishthaaror@users.noreply.github.com>
2022-09-03 22:55:49 +0530

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,14 @@
+
+function addUser() {
+  player1_name = document.getElementById("player1_name_input").value;
+  player2_name = document.getElementById("player2_name_input").value;
+
+    //Set "player1_name" using localStorage.setItem() function
+	  //Set "player2_name" using localStorage.setItem() function
+
+    //Get "player1_name" using localStorage.getItem() function and assign it to player1_name
+	  //Get "player2_name" using localStorage.getItem() function and assign it to player2_name
+
+    window.location = "game_page.html";
+}
+
